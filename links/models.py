bee49cd261bc8d2d7ed4c8f02312079faa49f9ce
Emmanuel Iduh <emmiduh93@gmail.com>
2022-06-27 22:56:43 +0100

    Created Link model

--- links/models.py
+++ links/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.template.defaultfilters import slugify
 
-# Create your models here.
+class Link(models.Model):
+    target_url = models.URLField(max_length=200)
+    description = models.CharField
+    identifier = models.SlugField(max_length=20, unique=True, blank=True)
+    author = models.ForeignKey(get_user_model(), null=True, on_delete=models.CASCADE,)
+    created_date = models.DateTimeField() 
+    active = models.BooleanField(default=True)
+    def __str__(self):
+        return self.title
