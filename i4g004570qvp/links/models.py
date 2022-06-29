08edbccc667ff1eadba0f3f6f34a74800684aaa5
Emmanuel Iduh <emmiduh93@gmail.com>
2022-06-29 01:41:51 +0100

    Updated models.py

--- i4g004570qvp/links/models.py
+++ i4g004570qvp/links/models.py
@@ -9,3 +9,8 @@ class Link(models.Model):
     author = models.ForeignKey(get_user_model(), null=True, on_delete=models.CASCADE,)
     created_date = models.DateTimeField() 
     active = models.BooleanField(default=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+        pass 
