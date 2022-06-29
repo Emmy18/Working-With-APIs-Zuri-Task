19850fe921d119e92d7054d1df5f06e8c1292d9b
Emmanuel Iduh <emmiduh93@gmail.com>
2022-06-29 01:46:04 +0100

    Update models.py

--- i4g004570qvp/links/models.py
+++ i4g004570qvp/links/models.py
@@ -4,7 +4,7 @@ from django.template.defaultfilters import slugify
 
 class Link(models.Model):
     target_url = models.URLField(max_length=200)
-    description = models.CharField(max_length=200)
+    description = models.CharField(max_length=200, default="Link Description")
     identifier = models.SlugField(max_length=20, unique=True, blank=True)
     author = models.ForeignKey(get_user_model(), null=True, on_delete=models.CASCADE,)
     created_date = models.DateTimeField() 
